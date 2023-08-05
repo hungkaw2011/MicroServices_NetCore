bab7d7f6bb3224517642a159330d7381f72e2fa8
hungkaw2011 <hungqaz38@gmail.com>
2023-08-05 19:20:08 +0700

    Basket + Redis

---
+++ Services/Basket/Basket.API/Entities/ShoppingCartItem.cs
@@ -0,0 +1,12 @@
+﻿namespace Basket.API.Entities
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+{
+    public class ShoppingCartItem
+    {
+        public int Quantity { get; set; }
+        public string Color { get; set; }
+        public decimal Price { get; set; }
+        public string ProductId { get; set; }
+        public string ProductName { get; set; }
+    }
+}
