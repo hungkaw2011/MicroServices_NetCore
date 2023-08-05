5ec97ab23ef083b5131ef47971b08568de57fdaf
hungkaw2011 <hungqaz38@gmail.com>
2023-08-05 14:31:08 +0700

    Add project files.

---
+++ Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,20 @@
+﻿using Catalog.API.Data.Interfaces;
+using Catalog.API.Entities;
+using MongoDB.Driver;
+using MongoDB.Driver.Core.Configuration;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+            var database = client.GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
+            var product = database.GetCollection<Product>(configuration.GetValue<string>("DatabaseSettings:CollectionName"));
+            CatalogContextSeed.SeedData(product);
+            Products = product; // Gán giá trị cho thuộc tính Products
+        }
+        public IMongoCollection<Product> Products { get; }
+    }
+}
---
+++ Services/Catalog/Catalog.API/Entities/Product.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+namespace Catalog.API.Entities
+{
+    public class Product
+    {
+        [BsonId] //Key 
+        [BsonRepresentation(BsonType.ObjectId)] //Xác định cách biểu diễn của thuộc tính khi ánh xạ vào BSON
+        public string Id { get; set; }
+
+        [BsonElement("Name")] // Ánh xạ thuộc tính Name vào trường có tên "name" trong MongoDB
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Summary { get; set; }
+        public string Description { get; set; }
+        public string ImageFile { get; set; }
+        public decimal Price { get; set; }
+    }
+}
