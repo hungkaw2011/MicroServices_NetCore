65949c1f7f8c754744144290a1dca729fb88d4c1
Hùng Nguyễn <hungqaz38@gmail.com>
2024-01-07 16:47:06 +0700

    SecondLife first commit

---
+++ Services/Catalog/Catalog.API/Controllers/MotorcycleController.cs
@@ -0,0 +1,24 @@
+﻿using Catalog.API.Entities.Vehicle;
+using Catalog.API.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Catalog.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]  
+    public class MotorcycleController : ControllerBase
+    {
+        private readonly IMotorcycleRepository _motorcycleRepository;
+        public MotorcycleController(IMotorcycleRepository motorcycleRepository)
+        {
+            _motorcycleRepository = motorcycleRepository;
+        }
+        [HttpGet]
+        public IEnumerable<Motorcycle> GetAllMotorcycle()
+        {
+            return _motorcycleRepository.GetAll();
+        }
+    }
+}
